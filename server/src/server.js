a6e4cb6830eee81891ce52a019b09700476d5199
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-11-22 09:24:44 +0530

    3: Remove the test route of server

--- server/src/server.js
+++ server/src/server.js
@@ -8,13 +8,6 @@ connectDB();
 
 const app = express();
 
-app.use("/api",(req,res)=>{
-    res.status(200).json({
-        success: true,
-        message: "Hello From Server"
-    })
-});
-
 app.listen(PORT,()=>{
     console.log(`Sever is running @loaclhost:${PORT}`);
 });
